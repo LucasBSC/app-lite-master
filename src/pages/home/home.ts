babfc076a917a9355d47183c2a566ccdc39afaab
Cassiano Vellames <c.vellames@outlook.com>
2018-01-04 21:03:48 -0300

    Suporte para multiplos carros

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -23,25 +23,28 @@ export class HomePage {
   @ViewChild('map') mapRef: ElementRef;
   map: any;
   events: Observable<any[]>;
-  currentMarker: any;
-  car: any;
+  currentMarkers: any[] = [];
+  cars: any[] = [];
 
   constructor(public navCtrl: NavController, public db: AngularFireDatabase, private afAuth: AngularFireAuth, private usersProvider: UsersProvider) {
-    var currentUser = this.usersProvider.findByUid(this.afAuth.auth.currentUser.uid);
-    console.log(this.afAuth.auth.currentUser);
-    currentUser.subscribe((users) => {
-      //console.log(users);
-      let userCars = users[0]['cars'];
-      Object.keys(userCars).map(key => { 
-        this.car = userCars[key];
-      });
-    })
+    
   }
 
   ionViewDidLoad() {
     // this.showMap();
     // this.printSelector();
-    this.loadGoogleMap();
+    //this.loadGoogleMap();
+    var currentUser = this.usersProvider.findByUid(this.afAuth.auth.currentUser.uid);
+    
+    currentUser.subscribe((users) => {
+      //console.log(users);
+      let userCars = users[0]['cars'];
+      Object.keys(userCars).map(key => { 
+        this.cars.push(userCars[key]);
+      });
+
+      this.loadGoogleMap();
+    })
   }
 
   loadGoogleMap() {
@@ -61,55 +64,72 @@ export class HomePage {
     this.map = GoogleMaps.create('map', mapOptions);
     this.map.one(GoogleMapsEvent.MAP_READY)
       .then(() => {
-        console.log('carimei', this.car);
-        this.db.list('/events', ref => ref.orderByChild('Imei').equalTo(this.car.Imei)).valueChanges()
-        .subscribe(result => {
-          // Recupera o ultimo tracker
-          console.log('events', result);
-          var lastTracker = null;
-          Object.keys(result).map(key => { 
-            if(result[key].Tipo.toLowerCase() == 'tracker') {
-              lastTracker = result[key];
+
+        if(this.cars.length > 1) {
+          this.map.setCameraZoom(11);
+        }
+
+        for(var i = 0; i < this.cars.length; i++) {
+          this.db.list('/events', ref => ref.orderByChild('Imei').equalTo(this.cars[i].Imei)).valueChanges()
+          .subscribe(result => {
+            // Recupera o ultimo tracker
+            console.log('events', result);
+            var lastTracker = null;
+            Object.keys(result).map(key => { 
+              if(result[key].Tipo.toLowerCase() == 'tracker') {
+                lastTracker = result[key];
+              }
+            });
+  
+            // Se nenhum evento de tracker for encontrado, a função é encerrada
+            if(!lastTracker) {
+              return;
             }
-          });
+            console.log(lastTracker);
 
-          // Se nenhum evento de tracker for encontrado, a função é encerrada
-          if(!lastTracker) {
-            return;
-          }
-          
-
-          this.map.clear()
-          // Remove o marker anterior
-          const latlng = {
-            lat: lastTracker.Latitude,
-            lng: lastTracker.Longitude
-          };
-          console.log('car', this.car);
-          console.log('event', lastTracker);
-          // Adiciona o novo marker
-          
-          this.map.animateCamera({
-            'target': latlng,
-          }, function() {
-            console.log("Camera position changed.");
-          });
-          this.map.addMarker({
-            title: this.car.Modelo + ' - ' + this.car.Cor + ' (' + this.car.Placa + ')',
-            icon: 'blue',
-            animation: 'DROP',
-            position: latlng
-          })
-          .then(marker => {
-            marker.on(GoogleMapsEvent.MARKER_CLICK)
-              .subscribe(() => {
-
+            const latlng = {
+              lat: lastTracker.Latitude,
+              lng: lastTracker.Longitude
+            };
+            
+            // Anima a camera apenas se existir um carro
+            if(this.cars.length == 1) {
+              this.map.animateCamera({
+                'target': latlng,
+              }, function() {
+                console.log("Camera position changed.");
               });
+            }
+            
+            // Adiciona marcador do carro
+            const car = this.findCarByImei(lastTracker.Imei);
+
+            if(this.currentMarkers[lastTracker.Imei]) {
+              this.currentMarkers[lastTracker.Imei].remove();
+            }
+
+            this.map.addMarker({
+              title: car.Modelo + ' ' + car.Cor + " - " + car.Ano +  ' (' + car.Placa + ')',
+              icon: 'blue',
+              animation: 'DROP',
+              position: latlng
+            })
+            .then(marker => {
+              this.currentMarkers[lastTracker.Imei] = marker;
+            });        
           });
-        });
+        }
+        
       });
   }
 
+  findCarByImei(imei: String) : any{
+    for(var i = 0; i < this.cars.length; i++) {
+      if(this.cars[i].Imei == imei) {
+        return this.cars[i];
+      }
+    }
+  }
   // showMap() {
   //   // location: Lat, Long
   //   const location = new google.maps.LatLng(-12.9995586,-38.5106808);
